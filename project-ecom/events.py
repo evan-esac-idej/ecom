73a85c847898b86e192c39f6d8e9b777b4d44516
Ginelio-Tchavana <151380096+evan-esac-idej@users.noreply.github.com>
2025-09-14 15:54:28 +0200

    Update events.py

--- project-ecom/events.py
+++ project-ecom/events.py
@@ -7,11 +7,22 @@ import os
 
 st.set_page_config(page_title='ecom_events', layout='wide', page_icon='bar_chart')
 
-
-base_dir = os.path.dirname(__file__)  
+# Caminho relativo do arquivo Excel baseado no script atual
+base_dir = os.path.dirname(__file__)          # pasta onde o script está
 file_path = os.path.join(base_dir, 'data_base', 'data.xlsx')
 
-data_base = pd.read_excel(file_path)
+# Checar se o arquivo existe
+if os.path.exists(file_path):
+    data_base = pd.read_excel(file_path)
+    st.success("Arquivo carregado com sucesso!")
+else:
+    st.warning("Arquivo não encontrado! Faça upload abaixo.")
+    
+    # Permitir upload do arquivo pelo usuário
+    uploaded_file = st.file_uploader("Escolha o arquivo Excel", type="xlsx")
+    if uploaded_file is not None:
+        data_base = pd.read_excel(uploaded_file)
+        st.success("Arquivo carregado com sucesso pelo upload!")
 
 
 
@@ -249,6 +260,7 @@ if button:
     placeholder.info('Desenvolvido por Ginélio Hermilio 🤠')
     sleep(2)
     placeholder.empty()
+
 
 
 
